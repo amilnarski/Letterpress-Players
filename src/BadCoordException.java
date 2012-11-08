9b04d0bca311948ba3b95ee55f14cb65f899d3d8
Aaron Miller <aaron@aaronmiller.in>
2012-11-08 15:01:08 -0500

    Custom Exception.

---
+++ src/BadCoordException.java
@@ -0,0 +1,23 @@
+
+public class BadCoordException extends Exception {
+
+	public BadCoordException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public BadCoordException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BadCoordException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public BadCoordException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+}
