a84c463e61d87d514b366279991638c243206c1f
Aaron Miller <aaron@aaronmiller.in>
2012-11-07 20:04:10 -0500

    Added element comments

--- src/Letterpress.java
+++ src/Letterpress.java
@@ -18,15 +18,15 @@ public class Letterpress {
 
 	private static Status[][] status;
 
-	/**
-	 * @param args
-	 */
-
 	public Letterpress() {
 		initializeBoard();
 		initializeDictionary();
 	}
 
+	/**
+	 * Initializes the game board with a random set of letters and sets each
+	 * letter to be NEUTRAL.
+	 */
 	private void initializeBoard() {
 		Letterpress.board = new char[5][5];
 		Random gen = new Random(0);
@@ -38,9 +38,15 @@ public class Letterpress {
 
 		Letterpress.status = new Status[5][5];
 		for (int i = 0; i < status.length; i++)
-			Arrays.fill(status[i], Status.NEUTRAL);
+			Arrays.fill(status[i], Status.NEUTRAL); // need to loop to do this
+													// since Arrays errors out
+													// on a matrix
 	}
 
+	/**
+	 * Reads in the words from pDictionary.txt and adds them to the in-memory
+	 * dictionary.
+	 */
 	private void initializeDictionary() {
 		Letterpress.dict = new HashSet<String>();
 		File dictionary = new File("pDictionary.txt");
@@ -57,13 +63,16 @@ public class Letterpress {
 		}
 	}
 
+	/**
+	 * Prints out the current board.
+	 */
 	public static void pBoard() {
 		for (int row = 0; row < 5; row++) {
 			String rowStr = "";
 			for (int col = 0; col < 5; col++) {
 				rowStr += board[row][col] + " ";
 			}
-			rowStr += "\t"+" ";
+			rowStr += "\t" + " ";
 			for (int col = 0; col < 5; col++) {
 				switch (status[row][col]) {
 				case NEUTRAL:
@@ -91,6 +100,9 @@ public class Letterpress {
 		p("");
 	}
 
+	/**
+	 * Prints out the current game board with indices to aid in debugging.
+	 */
 	public static void dBoard() {
 		System.out.println("  0 1 2 3 4\t  0 1 2 3 4");
 		for (int row = 0; row < 5; row++) {
@@ -98,7 +110,7 @@ public class Letterpress {
 			for (int col = 0; col < 5; col++) {
 				rowStr += board[row][col] + " ";
 			}
-			rowStr += "\t"+row + " ";
+			rowStr += "\t" + row + " ";
 			for (int col = 0; col < 5; col++) {
 				switch (status[row][col]) {
 				case NEUTRAL:
@@ -126,6 +138,10 @@ public class Letterpress {
 		p("");
 	}
 
+	/**
+	 * @param args
+	 * Generates a new Letterpress game.
+	 */
 	public static void main(String[] args) {
 		Letterpress lp = new Letterpress();
 		status[0][0] = Status.BLUE_DEFENDED;
@@ -135,12 +151,23 @@ public class Letterpress {
 		status[3][4] = Status.RED;
 		status[4][3] = Status.RED;
 		dBoard();
-		pBoard();
+		// pBoard();
 
 	}
 
+	/**
+	 * @param s
+	 * Makes printing that much easier.
+	 */
 	public static void p(Object s) {
 		System.out.println(s);
+	}
+	
+	/**
+	 * Makes printing that much easier.
+	 */
+	public static void p() {
+		System.out.println();
 	}
 
 }
