1cb248f9c1e8ad221707df475daa8d09a7bc7352
Aaron Miller <aaron@aaronmiller.in>
2012-11-08 01:30:44 -0500

    Made significant improvements to the dictionary script, removing several thousand false positives from the pDictionary.txt file.

--- dictionaryProcessor.py
+++ dictionaryProcessor.py
@@ -1,7 +1,7 @@
 import re
 #open the files
-f = open ('/Users/sit_down_you/Desktop/dictionary.txt','r')
-processedDict = open ('/Users/sit_down_you/Desktop/pDictionary.txt','a')
+f = open ('/Users/aaron/Desktop/dictionary.txt','r')
+processedDict = open ('/Users/aaron/Desktop/pDictionary.txt','a')
 
 #define the regex pattern for matching lines
 
@@ -10,11 +10,12 @@ lastMatch = ''
 for each in f:
 	#if there's a match, write the matched text to the file
 	line = f.next()
-	matchObj = re.match(r'^[A-Z][A-Z]+[^-*a-z0-9]?\b',line)
+	#matchObj = re.match(r'^[A-Z]{2,}[^-*\'\.a-z0-9]?\b',line)
+	matchObj = re.match(r'^[A-Z]{2,}\s?\n',line)
 	if (matchObj):
 		if (matchObj.group() != lastMatch):
 			processedDict.write(matchObj.group())
-			processedDict.write('\n')
+			#processedDict.write('\n')
 			lastMatch = matchObj.group()
 
 #close the files
